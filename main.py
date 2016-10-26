7335decb7b104af6fc0db2e66208241df4b0edab
ccssrryy <cs010@hotmail.com>
2016-10-26 16:15:52 +0800

    work ok

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 import socket
 
 import select
+import urllib2
 from cStringIO import StringIO
 from sets import Set
 
-port = 8001
+port = 8000
 ip = "0.0.0.0"
 wlist = Set()
 rlist = Set()
@@ -72,6 +73,7 @@ class ConnHandler(Handler, object):
             self.onConnectionLost()
             return
         path = "." + self.head_line.split(" ")[1]
+        path=urllib2.unquote(path)
         if not os.path.realpath(path).startswith(os.path.realpath("./")):
             self.socket.send("HTTP/1.1 405 forbidden\r\n\r\nsorrys!you have no permission to access the file path!")
             self.socket.close()
@@ -85,17 +87,17 @@ class ConnHandler(Handler, object):
         if os.path.isdir(path):
             folder, subs, fs = os.walk(path).next()
             body = ""
-            body += "<html> <title>%s</title><body>"
+            body += "<html><title>%s</title><body>"%path
             subs.append("..")
             subs.sort()
             for s in subs:
                 t = os.path.join(folder, s)
                 p = t.split("/")[-1]
-                body += "<a href='%s'><i>%s</i></a></br>" % (t, p)
+                body += "<a href='%s'><i>%s</i></a></br>" % (urllib2.quote(t), p)
             for f in fs:
                 t = os.path.join(folder, f)
                 p = t.split("/")[-1]
-                body += "<a href='%s'><b>%s</b></a></br>" % (t, p)
+                body += "<a href='%s'><b>%s</b></a></br>" % (urllib2.quote(t), p)
             body += "</body></html>"
             self.socket.send("HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s" % (len(body), body))
             self.socket.close()
@@ -111,7 +113,7 @@ class ConnHandler(Handler, object):
                 self.end = int(t1[1])
             else:
                 self.end = os.path.getsize(path) - 1
-            self._cachedSendData = "HTTP/1.1 206 partial\r\nContent-Length: %d\r\nContent-Range:bytes " \
+            self._cachedSendData = "HTTP/1.1 206 Partial Content\r\nContent-Length: %d\r\nContent-Range: bytes " \
                                    "%d-%d/%d\r\n" \
                                    "Accept-Ranges: bytes\r\n\r\n" % (self.end - self.start + 1,self.start,self.end, os.path.getsize(path))
         else:
