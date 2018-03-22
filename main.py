7f60dbebe986e3f851dc365cfc3a0b83433e1bdc
cs <cs010@hotmail.com>
2018-03-22 18:28:12 +0800

    add multiprocess support

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ parser.add_argument('-d', '--dir', help='work dir', type=str, default='./')
 parser.add_argument('-l ', '--loglevel', help='log level', type=str, default='DEBUG')
 parser.add_argument('-u', '--user', help='username', type=str, default='admin')
 parser.add_argument('-k', '--key', help='key', type=str, default='admin')
+parser.add_argument('-c', '--concurrency', help='the number of workers', type=int, default=3)
 args = parser.parse_args()
 user = args.user
 key = args.key
@@ -29,6 +30,7 @@ socket_handler_dict = dict()  # type:{socket.socket:Handler}
 logging.basicConfig(level=getattr(logging, args.loglevel),
                     format="%(asctime)s - [%(levelname)s]: %(message)s")
 _logger = logging.getLogger()
+
 
 class Handler(object):
     def __init__(self, soc):
@@ -48,8 +50,8 @@ class Handler(object):
 
 
 class ConnHandler(Handler, object):
-    def __init__(self,soc):
-        super(ConnHandler,self).__init__(soc)
+    def __init__(self, soc):
+        super(ConnHandler, self).__init__(soc)
         self.requestLine = ''  # type: str
         self._cachedRecvData = ''  # type: str
         self._cachedSendData = ''  # type: str
@@ -61,18 +63,21 @@ class ConnHandler(Handler, object):
         self.size_sent = 0  # type:int
         self.peername = ()
 
+    def close(self):
+        self.socket.close()
+        self.onConnectionLost()
+
     def onConnectionMade(self):
         _logger.debug("connection made from %s", self.socket.getpeername())
         self.peername = self.socket.getpeername()
 
     def onError(self, e):
-        _logger.debug(e)
+        _logger.error(e)
 
     def onDataRecv(self, data):
         if len(self._cachedRecvData) > 2048:
             self.socket.send("HTTP/1.1 405 forbidden\r\n\r\nforbidden,header is too long")
-            self.socket.close()
-            self.onConnectionLost()
+            self.close()
             return
         self._cachedRecvData += data
         index = self._cachedRecvData.find("\r\n\r\n")
@@ -94,26 +99,26 @@ class ConnHandler(Handler, object):
             k = base64.b64decode(k).split(":")
             if k[0] != user or k[1] != key:
                 self.socket.send("HTTP/1.1 401 Unauthorized\r\nWWW-Authenticate: Basic\r\n\r\n")
+                self.socket.close()
+                self.onConnectionLost()
                 return
         else:
             self.socket.send("HTTP/1.1 401 Authorization Required\r\nWWW-Authenticate: Basic\r\n\r\n")
             return
         if not self.head_line.startswith("GET"):
             self.socket.send("HTTP/1.1 405 forbidden\r\n\r\nsorrys!request method is not supported!")
-            self.socket.close()
-            self.onConnectionLost()
+            self.close()
             return
         path = "." + self.head_line.split(" ")[1]
-        path=urllib2.unquote(path)
+        path = urllib2.unquote(path)
         if not os.path.realpath(path).startswith(os.path.realpath("./")):
             self.socket.send("HTTP/1.1 405 forbidden\r\n\r\nsorrys!you have no permission to access the file path!")
-            self.socket.close()
-            self.onConnectionLost()
+            self.close()
             return
         if not os.path.exists(path):
-            self.socket.send("HTTP/1.1 404 Not Found\r\n\r\n<html><title>404</title><body>404 not found:%s</body></html>"%path)
-            self.socket.close()
-            self.onConnectionLost()
+            self.socket.send(
+                "HTTP/1.1 404 Not Found\r\n\r\n<html><title>404</title><body>404 not found:%s</body></html>" % path)
+            self.close()
             return
         if os.path.isdir(path):
             folder, subs, fs = os.walk(path).next()
@@ -132,15 +137,16 @@ class ConnHandler(Handler, object):
             for f in fs:
                 t = os.path.join(folder, f)
                 p = t.split("/")[-1]
-                mim=mimetypes.guess_type(p)[0]
-                if False:#mim.find("video")!=-1:
-                    body+="<h3>%s</h3><video title='%s' controls ><source src=\"%s\" type=\"%s\"></video></br>"%(p,p,urllib2.quote(t),mim)
+                mim = mimetypes.guess_type(p)[0]
+                if False:  # mim.find("video")!=-1:
+                    body += "<h3>%s</h3><video title='%s' controls ><source src=\"%s\" type=\"%s\"></video></br>" % (
+                    p, p, urllib2.quote(t), mim)
                 else:
-                    body += "<a href='%s'><b>%s</b><i>(%s)</i></a></br>" % (urllib2.quote(t)[1:], p,m_size(os.path.getsize(t)))
+                    body += "<a href='%s'><b>%s</b><i>(%s)</i></a></br>" % (
+                    urllib2.quote(t)[1:], p, m_size(os.path.getsize(t)))
             body += "</body></html>"
             self.socket.send("HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s" % (len(body), body))
-            self.socket.close()
-            self.onConnectionLost()
+            self.close()
             return
 
         ran = self.header.get("Range", None)
@@ -154,7 +160,8 @@ class ConnHandler(Handler, object):
                 self.end = os.path.getsize(path) - 1
             self._cachedSendData = "HTTP/1.1 206 Partial Content\r\nContent-Length: %d\r\nContent-Range: bytes " \
                                    "%d-%d/%d\r\n" \
-                                   "Accept-Ranges: bytes\r\n\r\n" % (self.end - self.start + 1,self.start,self.end, os.path.getsize(path))
+                                   "Accept-Ranges: bytes\r\n\r\n" % (
+                                   self.end - self.start + 1, self.start, self.end, os.path.getsize(path))
         else:
             self._cachedSendData = "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes\r\nContent-Length: %d\r\n\r\n" % os.path.getsize(
                 path)
@@ -165,7 +172,7 @@ class ConnHandler(Handler, object):
     def onConnectionLost(self):
         if self.file:
             self.file.close()
-        _logger.debug("connection lost %s",self.peername)
+        _logger.debug("connection lost %s", self.peername)
         try:
             wlist.remove(self.socket)
         except:
@@ -182,18 +189,18 @@ class ConnHandler(Handler, object):
             self._cachedSendData = self._cachedSendData[size:]
             if size != l:
                 return
-        if self.end==-1:
-            c=self.file.read(2048)
+        if self.end == -1:
+            c = self.file.read(2048)
         else:
             more = self.end - self.start + 1 - self.size_sent
             c = self.file.read(min(2048, more))
         if not c:
-            self.socket.close()
-            self.onConnectionLost()
+            self.close()
             return
         try:
             size = self.socket.send(c)
-        except:
+        except Exception as e:
+            _logger.error(e.args)
             self.onConnectionLost()
             return
         self.size_sent += size
@@ -202,26 +209,47 @@ class ConnHandler(Handler, object):
 
 
 server = socket.socket()
+
+
+def shutdown(f):
+    def _wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            _logger.error(e)
+            server.close()
+    return _wrapper
+
+
 def close_server(*args):
     server.close()
     _logger.debug("server closed")
 
+
 def init_signal():
     signal.signal(signal.SIGINT, close_server)
 
+
+@shutdown
 def main():
     init_signal()
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((ip, port))
-    server.setblocking(False)
     server.listen(5)
     rlist.add(server)
+    for i in range(0, args.concurrency):
+        pid = os.fork()
+        if pid == 0:  # child process
+            break
+        else:
+            _logger.info("starting worker-[%s]", pid)
     while True:
         rs, ws, xs = select.select(rlist, wlist, [])
         if rs:
             for r in rs:
                 if r == server:
                     con, addr = r.accept()
+                    con.setblocking(0)
                     socket_handler_dict[con] = ConnHandler(con)  # type:Handler
                     rlist.add(con)
                     # connection made
@@ -238,7 +266,7 @@ def main():
                     # process data
                     socket_handler_dict[r].onDataRecv(data)
                 except Exception as e:
-                    _logger.debug(e.args)
+                    _logger.error(e.args)
         if ws:
             for w in ws:
                 socket_handler_dict[w].onReadyWrite()
@@ -249,13 +277,14 @@ def m_size(size):
     if size == 0:
         return '0b'
     r = ''
-    for i in range(0,4):
+    for i in range(0, 4):
         e = size % 1024
         r = str(e) + a[i] + r
         size >>= 10
         if size == 0:
             break
     return r
+
 
 if __name__ == "__main__":
     main()
